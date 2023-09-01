d3d265318200de68c6dbfe62bac40ad90de4daa6
Luis Ricardo <luiscastrodev@gmail.com>
2023-09-01 11:57:01 -0300

    init progress

--- app/src/main/java/com/example/jettimer/Timer.kt
+++ app/src/main/java/com/example/jettimer/Timer.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +22,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -64,15 +67,56 @@ fun Timer(
                 )
             }, label = ""
         ) { time ->
-            Text(text = getFormattedTime(time),
+            Text(
+                text = getFormattedTime(time),
                 style = MaterialTheme.typography.bodyLarge,
                 fontWeight = FontWeight.Bold
             )
         }
     }
+}
 
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+private fun JetTimerProgressIndicator(
+    progress: Float,
+    currentTime: Long
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
 
+        AnimatedContent(
+            targetState = currentTime,
+            transitionSpec = {
+                if (targetState > initialState) {
+                    slideInVertically { fullHeight -> fullHeight } + fadeIn() with
+                            slideOutVertically { fullHeight -> fullHeight } + fadeOut()
+                } else {
+                    slideInVertically { fullHeight -> fullHeight } + fadeIn() with
+                            slideOutVertically { fullHeight -> fullHeight } + fadeOut()
+                }.using(
+                    sizeTransform = SizeTransform(clip = false)
+                )
+            }, label = ""
+        ) { time ->
+            Text(
+                text = getFormattedTime(time),
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold
+            )
+        }
 
+    }
+}
+
+@Composable
+private fun CircularIndicator(progress: Float) {
+    Surface(color =  MaterialTheme.colorScheme.background) {
+        val stroke = with(LocalDensity.current){
+        }
+    }
 }
 
 @Preview(
